51a4f135936f5f7bce9a510814708844708bf4bd
Tylryan <tlrrn5@gmail.com>
2021-05-04 11:32:26 -0500

    Installer should work

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ cd ~/
 # Clone My Preset configs 
 git clone https://github.com/Tylryan/zsh_config.git ~/.config/zsh/
 
-# Copy them to your home directory.
-cp ~/zsh_config/.p10k.zsh 
-cp ~/zsh_config/.zshrc
+# Copy the configs to your home directory.
+cp ~/zsh_config/.p10k.zsh ~/
+cp ~/zsh_config/.zshrc ~/
 
 # Removing the clone.
 rm -rf zsh_config
@@ -19,3 +19,5 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ~/.config/zsh/plugins
 
 # Powerlevel10k: Customized PS1
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.config/zsh/plugins/powerlevel10k
+
+bash
