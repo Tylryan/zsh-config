eff4d3f165a95ca412608636373db7817a69c68c
Tylryan <tlrrn5@gmail.com>
2021-05-04 11:12:51 -0500

    Added an install file

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+cd ~/
+
+# Clone My Preset configs 
+git clone https://github.com/Tylryan/zsh_config.git ~/.config/zsh/
+
+# Copy them to your home directory.
+cp ~/.config/zsh/.p10k.zsh .zshrc ~/
+
+# Syntax Highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/zsh/plugins/zsh-syntax-highlighting
+
+# Auto-Suggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.config/zsh/plugins/zsh-autosuggestions
+
+# Powerlevel10k: Customized PS1
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.config/zsh/plugins/powerlevel10k
+
+
+
