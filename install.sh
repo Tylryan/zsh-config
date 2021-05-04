99ebd8080aa01a978dbf5d1c7b2dde8db41e652f
Tylryan <tlrrn5@gmail.com>
2021-05-04 11:19:48 -0500

    Install.sh does everything except install zsh for you. Or at least it should.

--- install.sh
+++ install.sh
@@ -7,6 +7,9 @@ git clone https://github.com/Tylryan/zsh_config.git ~/.config/zsh/
 # Copy them to your home directory.
 cp ~/.config/zsh/.p10k.zsh .zshrc ~/
 
+# Removing the clone.
+rm -rf zsh_config
+
 # Syntax Highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/zsh/plugins/zsh-syntax-highlighting
 
@@ -16,5 +19,5 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ~/.config/zsh/plugins
 # Powerlevel10k: Customized PS1
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.config/zsh/plugins/powerlevel10k
 
-
+source ~/.zshrc
 
