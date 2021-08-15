538bbb7178a36dbb36e1d6964f7e342701e4e5e2
Tyler Ryan <tlrrn5@gmail.com>
2021-08-15 16:50:35 -0500

    Fixing up stuff

--- install.sh
+++ install.sh
@@ -1,23 +1,20 @@
 #!/usr/bin/bash
 cd ~/
 
-# Clone My Preset configs 
-git clone https://github.com/Tylryan/zsh_config.git ~/.config/zsh/
 
 # Copy the configs to your home directory.
-cp ~/zsh_config/.p10k.zsh ~/
-cp ~/zsh_config/.zshrc ~/
+cp ~/zsh/.p10k.zsh ~/
+cp ~/zsh/.zshrc ~/
 
-# Removing the clone.
-rm -rf zsh_config
+mv zsh ~/.config/zsh/
 
+ln ~/.config/zsh/.zshrc ~/
 # Syntax Highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/zsh/plugins/zsh-syntax-highlighting
+#git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/zsh/plugins/zsh-syntax-highlighting
 
 # Auto-Suggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.config/zsh/plugins/zsh-autosuggestions
+#git clone https://github.com/zsh-users/zsh-autosuggestions ~/.config/zsh/plugins/zsh-autosuggestions
 
 # Powerlevel10k: Customized PS1
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.config/zsh/plugins/powerlevel10k
+#git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.config/zsh/plugins/powerlevel10k
 
-bash
