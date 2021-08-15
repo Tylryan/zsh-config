b47d30db5496eeb22662e16b08eb7f1f3743468e
Tyler Ryan <tlrrn5@gmail.com>
2021-08-15 16:57:48 -0500

    Now i think i've fixed it

--- install.sh
+++ install.sh
@@ -6,15 +6,16 @@ cd ~/
 cp ~/zsh/.p10k.zsh ~/
 cp ~/zsh/.zshrc ~/
 
+ln ~/zsh/.zshrc ~/
+
 mv zsh ~/.config/zsh/
 
-ln ~/.config/zsh/.zshrc ~/
 # Syntax Highlighting
-#git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/zsh/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/zsh/plugins/zsh-syntax-highlighting
 
 # Auto-Suggestions
-#git clone https://github.com/zsh-users/zsh-autosuggestions ~/.config/zsh/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.config/zsh/plugins/zsh-autosuggestions
 
 # Powerlevel10k: Customized PS1
-#git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.config/zsh/plugins/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.config/zsh/plugins/powerlevel10k
 
